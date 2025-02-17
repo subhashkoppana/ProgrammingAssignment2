b9a65778e93f3e2a6d53cf57249ff212c83fb508
Subhash Koppana <subhash.thegeek@outlook.com>
2025-02-17 12:16:59 +0530

    completed prg assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ invm <- NULL
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) invm <<- inverse
+  getInverse <- function() invm
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invm <- x$getInverse()
+  if (!is.null(invm)) {
+    return(invm)
+  }
+  m <- x$get()
+  invm <- solve(m, ...)
+  x$setInverse(invm)
+  invm
 }
